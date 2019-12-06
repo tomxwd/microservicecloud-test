f8be356a09d57e3c51834cec214ce90a9602d0d8
tomxwd <648913416@qq.com>
2019-12-06 23:23:23 +0800

    新增microservicecloud-eureka-7001项目,完成Eureka注册中心服务端的搭建

---
+++ microservicecloud-eureka-7001/src/main/java/top/tomxwd/springcloud/EurekaServer7001_App.java
@@ -0,0 +1,20 @@
+package top.tomxwd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author xieweidu
+ * @createDate 2019-12-06 23:03
+ */
+@SpringBootApplication
+// EurekaServer服务端启动类，接受其他微服务注册进来
+@EnableEurekaServer
+public class EurekaServer7001_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7001_App.class,args);
+    }
+
+}

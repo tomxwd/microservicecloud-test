f7cb524552f907cabbade7a5da8a55d94f008940
tomxwd <648913416@qq.com>
2019-12-07 17:30:08 +0800

    eureka集群搭建

--- microservicecloud-provider-dept-8001/src/main/java/top/tomxwd/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/top/tomxwd/springcloud/DeptProvider8001_App.java
@@ -2,6 +2,7 @@ package top.tomxwd.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -11,6 +12,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 // 本服务启动后会自动注册进Eureka服务中
 @EnableEurekaClient
+// 服务发现
+@EnableDiscoveryClient
 public class DeptProvider8001_App {
 
     public static void main(String[] args) {

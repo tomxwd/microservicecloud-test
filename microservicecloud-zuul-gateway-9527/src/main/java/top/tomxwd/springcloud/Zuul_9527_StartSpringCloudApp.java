ca477ee50c2a5436fdfc9298116fa67aeda7e814
tomxwd <648913416@qq.com>
2019-12-08 21:14:27 +0800

    Zuul路由网关整合

---
+++ microservicecloud-zuul-gateway-9527/src/main/java/top/tomxwd/springcloud/Zuul_9527_StartSpringCloudApp.java
@@ -0,0 +1,19 @@
+package top.tomxwd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author xieweidu
+ * @createDate 2019-12-08 20:30
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul_9527_StartSpringCloudApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul_9527_StartSpringCloudApp.class, args);
+    }
+
+}

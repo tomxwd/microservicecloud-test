2f1c37ca7e83724698ea09fd3bb26251a69670ce
tomxwd <648913416@qq.com>
2019-12-08 17:48:22 +0800

    Hystrix服务熔断整合

--- microservicecloud-provider-dept-hystrix-8001/src/main/java/top/tomxwd/springcloud/DeptProvider8001_Hystrix_App.java
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/top/tomxwd/springcloud/DeptProvider8001_Hystrix_App.java
@@ -2,6 +2,7 @@ package top.tomxwd.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -14,10 +15,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 // 服务发现
 @EnableDiscoveryClient
-public class DeptProvider8001_App {
+// 对Hystrix熔断机制的支持
+@EnableCircuitBreaker
+public class DeptProvider8001_Hystrix_App {
 
     public static void main(String[] args) {
-        SpringApplication.run(DeptProvider8001_App.class,args);
+        SpringApplication.run(DeptProvider8001_Hystrix_App.class,args);
     }
 
 }
--- microservicecloud-provider-dept-hystrix-8001/src/main/java/top/tomxwd/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/top/tomxwd/springcloud/controller/DeptController.java
@@ -1,5 +1,6 @@
 package top.tomxwd.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -17,35 +18,22 @@ import java.util.List;
 public class DeptController {
 
     @Autowired
-    private DeptService service;
-    @Autowired
-    private DiscoveryClient client;
-
-    @PostMapping(value = "/dept/add")
-    public boolean add(@RequestBody Dept dept) {
-        return service.add(dept);
-    }
+    private DeptService service = null;
 
     @GetMapping(value = "/dept/get/{id}")
+    // 一旦调用服务方法失败并抛出了错误信息之后，会自动调用@HystrixCommand标注好的fallbackMethod调用类中的指定方法
+    @HystrixCommand(fallbackMethod = "processHystrix_Get")
     public Dept get(@PathVariable("id") Long id) {
-        return service.get(id);
+        Dept dept = this.service.get(id);
+        if (null == dept) {
+            throw new RuntimeException("该ID: " + id + " 没有找到对应的信息");
+        }
+        return dept;
     }
 
-    @GetMapping(value = "/dept/list")
-    public List<Dept> list() {
-        return service.list();
-    }
-
-    @GetMapping(value = "/dept/discovery")
-    public Object discovery() {
-        List<String> list = client.getServices();
-        System.out.println("**************************" + list);
-        List<ServiceInstance> serviceInstanceList = client.getInstances("MICROSERVICECLOUD-DEPT");
-        serviceInstanceList.stream().forEach(serviceInstance -> {
-            System.out.println(serviceInstance.getServiceId() + "\t" + serviceInstance.getHost() + "\t"
-                    + serviceInstance.getPort() + "\t" + serviceInstance.getUri());
-        });
-        return this.client;
+    public Dept processHystrix_Get(@PathVariable("id") Long id) {
+        return new Dept().setDeptno(id).setDname("该ID: " + id + "没有找到对应的信息,null--@HystrixCommand")
+                .setDb_source("no this database in MySQL");
     }
 
 }

f411f2a00ba34f19490a537b28e5281ab7a2792f
tomxwd <648913416@qq.com>
2019-12-10 23:46:06 +0800

    配置中心 建立客户端项目，拉取GitHub配置文件部署项目

---
+++ microservicecloud-config-client-3355/src/main/java/top/tomxwd/springcloud/ConfigClient_3355_StartSpringCloudApp.java
@@ -0,0 +1,15 @@
+package top.tomxwd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xieweidu
+ * @createDate 2019-12-10 23:29
+ */
+@SpringBootApplication
+public class ConfigClient_3355_StartSpringCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient_3355_StartSpringCloudApp.class, args);
+    }
+}
